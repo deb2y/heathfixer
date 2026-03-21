aba06dae13ea05dbd396fe64284b7959ec9b06b5
[$] deb2y <112425339+deb2y@users.noreply.github.com>
2026-03-21 15:33:32 +0300

    Add files via upload

---
+++ src/main/java/com/deb2y/healthfixer/HealthFixer.java
@@ -0,0 +1,9 @@
+package com.deb2y.healthfixer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HealthFixer implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
