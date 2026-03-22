c89112cbbcf12eb754b5e86ef6b34737061ab202
[$] deb2y <112425339+deb2y@users.noreply.github.com>
2026-03-22 16:30:56 +0300

    Delete src/main directory

--- src/main/java/com/deb2y/healthfixer/HealthFixer.java
+++
@@ -1,9 +0,0 @@
-package com.deb2y.healthfixer;
-
-import net.fabricmc.api.ModInitializer;
-
-public class HealthFixer implements ModInitializer {
-    @Override
-    public void onInitialize() {
-    }
-}
